74922aff52a5a6254c7b431eb2258ac19ece657c
Gulnoza Kh <36320417+khodizoda@users.noreply.github.com>
2020-06-17 18:46:19 -0400

    spacing fixed

--- get_next_line.h
+++ get_next_line.h
@@ -16,11 +16,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
-char	*ft_strncpy(char *dst, char *src, size_t len);
-char	*ft_strdup(char *s1);
-int		is_nl(char *s);
-size_t	line_len(char *str);
-size_t	buff_len(char *str);
+int       get_next_line(int fd, char **line);
+char      *ft_strncpy(char *dst, char *src, size_t len);
+char      *ft_strdup(char *s1);
+int       is_nl(char *s);
+size_t    line_len(char *str);
+size_t    buff_len(char *str);
 
 #endif
